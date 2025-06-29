635eebe5127bd5ece8f1bb1bd1f7381e249bc300
dean <dean@stclab.com>
2025-06-29 15:32:10 +0900

    chore: env 파일 config로 적용

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 // next.config.js
+require('dotenv').config({ path: '.env' });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     swcMinify: true,
     compiler: {
         styledComponents: true,
     },
+    env: {
+        NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+        NEXT_PUBLIC_NAVER_ID: process.env.NEXT_PUBLIC_NAVER_ID,
+    }
 };
 
 module.exports = nextConfig;
