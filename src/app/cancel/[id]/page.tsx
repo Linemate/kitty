5db996549e400ac5c79300188e758f6ca44a417a
윤지은 <yoonzeen@natecorp.com>
2025-07-07 18:00:47 +0900

    [MOD] header lang 삭제

--- src/app/cancel/[id]/page.tsx
+++ src/app/cancel/[id]/page.tsx
@@ -8,18 +8,21 @@ import 'styles/cancelPage.scss';
 const options = [
     {
         id: 1,
-        text: '개인 일정 변경'
-    }, {
+        text: '개인 일정 변경',
+    },
+    {
         id: 2,
-        text: '단순 변심'
-    }, {
+        text: '단순 변심',
+    },
+    {
         id: 3,
-        text: '모임 내용 변경으로 인한 참여불가'
-    }, {
+        text: '모임 내용 변경으로 인한 참여불가',
+    },
+    {
         id: 4,
-        text: '오랜 대기 문제'
-    }
-]
+        text: '오랜 대기 문제',
+    },
+];
 const Cancel = () => {
     const maxLength = 200;
     const inputRef = useRef<HTMLDivElement>(null);
@@ -42,21 +45,21 @@ const Cancel = () => {
     // 선택창 자세히 보기 on/off
     const handleOpenSelectOptions = () => {
         setOpenSelectOptions(!openSelectOptions);
-    }
+    };
 
     // 선택창에서 선택
-    const handleSelect = (id:number, text:string) => {
+    const handleSelect = (id: number, text: string) => {
         setSelectedReasonId(id);
         setSelectedReasonText(text);
         setDetailReason('');
         setOpenInputOfEtc(false);
-    }
+    };
 
     // 기타 선택
     const selectEtc = () => {
         handleOpenSelectOptions();
         setOpenInputOfEtc(true);
-    }
+    };
 
     // 기타 - 상세 사유 입력
     const handleInput = () => {
@@ -64,20 +67,20 @@ const Cancel = () => {
             const text = inputRef.current.innerText;
             const trimmedText = text.substring(0, maxLength);
             if (text.length > maxLength) {
-              inputRef.current.innerText = trimmedText;
-              setDetailReason(trimmedText);
-      
-              // 커서를 끝으로 이동
-              const range = document.createRange();
-              const sel = window.getSelection();
-              range.selectNodeContents(inputRef.current);
-              range.collapse(false);
-              if (sel) {
-                sel.removeAllRanges();
-                sel.addRange(range);
-              }
+                inputRef.current.innerText = trimmedText;
+                setDetailReason(trimmedText);
+
+                // 커서를 끝으로 이동
+                const range = document.createRange();
+                const sel = window.getSelection();
+                range.selectNodeContents(inputRef.current);
+                range.collapse(false);
+                if (sel) {
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
             } else {
-              setDetailReason(text);
+                setDetailReason(text);
             }
         }
     };
@@ -86,110 +89,94 @@ const Cancel = () => {
     const inputDetails = () => {
         setActiveDetailReason(true);
         inputRef.current?.focus();
-    }
+    };
 
     // check on/off
     const handleChecked = () => {
         setIsChecked(!isChecked);
-    }
+    };
 
     return (
-        <div className='cancel'>
-            <div className='wrapper'>
+        <div className="cancel">
+            <div className="wrapper">
                 {/* Header */}
-                <Header title={'모임 대기 취소'} isDepth={true} lang={'ko'} />
-                <div className='contents'>
-                    <div className='section'>
-                        <div className='sub_title'>모임 정보</div>
-                        <div className='desc'>
+                <Header title={'모임 대기 취소'} isDepth={true} />
+                <div className="contents">
+                    <div className="section">
+                        <div className="sub_title">모임 정보</div>
+                        <div className="desc">
                             <ProgramInMypage id={4} name={'MAKE A TRADITIONAL FOOD WITH KOREAN FRIENDS'} status={'attended'} applyDate={'02.12(Mon)'} date={'2024.02.12(Mon) 1:00 PM '} location={'Gangnam Station'} type={'simple'} />
                         </div>
                     </div>
-                    <div className='section'>
-                        <div className='sub_title'>
+                    <div className="section">
+                        <div className="sub_title">
                             <div>
-                                취소 사유<span className='text_red2'>*</span>
+                                취소 사유<span className="text_red2">*</span>
                             </div>
                         </div>
-                        <div className='desc'>
-                            <div className='select_wrap' onClick={handleOpenSelectOptions}>
-                                <div className='select_text_wrap'>
-                                    <div className={`ico arrow_bottom gray ${selectedReasonId === 0 ? 'default' : 'selected'}`}>
-                                        {
-                                            selectedReasonId === 0 ? 
-                                            '사유를 선택해주세요.' : selectedReasonText
-                                        }
-                                    </div>
+                        <div className="desc">
+                            <div className="select_wrap" onClick={handleOpenSelectOptions}>
+                                <div className="select_text_wrap">
+                                    <div className={`ico arrow_bottom gray ${selectedReasonId === 0 ? 'default' : 'selected'}`}>{selectedReasonId === 0 ? '사유를 선택해주세요.' : selectedReasonText}</div>
                                 </div>
-                                {
-                                    openSelectOptions &&
-                                    <div className='select_options'>
+                                {openSelectOptions && (
+                                    <div className="select_options">
                                         <ul>
-                                            {
-                                                options.map((el:any) => 
+                                            {options.map((el: any) => (
                                                 <li key={el.id} onClick={() => handleSelect(el.id, el.text)}>
-                                                    <div className='option'>
-                                                        {el.text}
-                                                    </div>
-                                                </li>)
-                                            }
-                                            <li onClick={selectEtc}><div className='option'>기타</div></li>
+                                                    <div className="option">{el.text}</div>
+                                                </li>
+                                            ))}
+                                            <li onClick={selectEtc}>
+                                                <div className="option">기타</div>
+                                            </li>
                                         </ul>
                                     </div>
-                                }
+                                )}
                             </div>
                         </div>
                     </div>
-                    {
-                        openInputOfEtc && 
-                        <div className='section'>
-                            <div className='sub_title'>
+                    {openInputOfEtc && (
+                        <div className="section">
+                            <div className="sub_title">
                                 <div>
-                                    상세 사유<span className='text_red2'>*</span>
+                                    상세 사유<span className="text_red2">*</span>
                                 </div>
                                 <div>
-                                    <span className='letters'><span className='current'>{detailReason.length}</span>/{maxLength}</span>
+                                    <span className="letters">
+                                        <span className="current">{detailReason.length}</span>/{maxLength}
+                                    </span>
                                 </div>
                             </div>
-                            <div className='input_wrap'>
-                                <div className='input' ref={inputRef} contentEditable onInput={handleInput} suppressContentEditableWarning onCompositionStart={handleInput}
-                                onCompositionEnd={handleInput} />
-                                {
-                                    !activeDetailReason && 
-                                    <div className='input_helper' onClick={inputDetails}>취소하는 상세한 사유를 입력해주세요.</div>
-                                }
+                            <div className="input_wrap">
+                                <div className="input" ref={inputRef} contentEditable onInput={handleInput} suppressContentEditableWarning onCompositionStart={handleInput} onCompositionEnd={handleInput} />
+                                {!activeDetailReason && (
+                                    <div className="input_helper" onClick={inputDetails}>
+                                        취소하는 상세한 사유를 입력해주세요.
+                                    </div>
+                                )}
                             </div>
                         </div>
-                    }
-                    <div className='section price_wrap'>
-                        <div className='calculate_price'>
-                            <div className='row'>
-                                <div className='cate'>
-                                    결제 금액
-                                </div>
-                                <div className='price'>
-                                    45,000원
-                                </div>
+                    )}
+                    <div className="section price_wrap">
+                        <div className="calculate_price">
+                            <div className="row">
+                                <div className="cate">결제 금액</div>
+                                <div className="price">45,000원</div>
                             </div>
-                            <div className='row border_bottom'>
-                                <div className='cate'>
-                                    취소 수수료
-                                </div>
-                                <div className='price'>
-                                    0원
-                                </div>
+                            <div className="row border_bottom">
+                                <div className="cate">취소 수수료</div>
+                                <div className="price">0원</div>
                             </div>
-                            <div className='row total'>
-                                <div className='cate'>환불 금액</div>
-                                <div className='price'>
-                                    45,000원
-                                </div>
+                            <div className="row total">
+                                <div className="cate">환불 금액</div>
+                                <div className="price">45,000원</div>
                             </div>
                         </div>
                     </div>
-                    <div className='section refund_wrap'>
-                        <div className='sub_title'>환불 규정 안내</div>
-                        <div className='dots'>
+                    <div className="section refund_wrap">
+                        <div className="sub_title">환불 규정 안내</div>
+                        <div className="dots">
                             <ul>
                                 <li>결제 후 30분 경과 전 : 전액 환불</li>
                                 <li>승인 대기 중인 상태에서 신청 취소한 경우 : 전액 환불</li>
@@ -199,17 +186,14 @@ const Cancel = () => {
                                 <li>모임 진행 당일에 신청한 경우 : 환불 불가 </li>
                             </ul>
                         </div>
-                        <div className='ico info'>
-                            모임 일자가 임박하여 취소하는 경우 취소승인이 불가할 수 있으며, 시작 3일 전에는 환불불가인 점 안내드립니다. 결제 승인 취소는 영업일 기준 3~5일 소요될 수 있어요. 
-                        </div>
+                        <div className="ico info">모임 일자가 임박하여 취소하는 경우 취소승인이 불가할 수 있으며, 시작 3일 전에는 환불불가인 점 안내드립니다. 결제 승인 취소는 영업일 기준 3~5일 소요될 수 있어요.</div>
                     </div>
-                    <div className='section check_wrap'>
+                    <div className="section check_wrap">
                         <div className={`ico checkbox ${isChecked ? 'checked' : 'default'}`} onClick={handleChecked}>
-                        환불 규정을 확인했으며, 이에 동의합니다.
+                            환불 규정을 확인했으며, 이에 동의합니다.
                         </div>
                     </div>
-                    <div className={`cancel btn text ${isBtnActive ? 'bg_blue' : 'bg_gray'}`}>모임 취소하기
-                    </div>
+                    <div className={`cancel btn text ${isBtnActive ? 'bg_blue' : 'bg_gray'}`}>모임 취소하기</div>
                 </div>
             </div>
         </div>
--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import { TextButtonWithIcon } from 'components/common/Button';
@@ -8,45 +8,52 @@ import React from 'react';
 export default function MypageLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter();
     // 페이지 이동
-    const viewPage = (pageName:string) => {
+    const viewPage = (pageName: string) => {
         router.push(`/mypage/${pageName}`);
-    }
+    };
     return (
-        <div className='mypage'>
-            <div className='wrapper'>
+        <div className="mypage">
+            <div className="wrapper">
                 {/* Header & Key visual */}
-                <Header title={'라인메이트 메인'} lang={'ko'} />
-                <div className='my_info'>
-                    <div className='my_info_inner'>
-                        <div className='img_area'>
-                            <div className='none'></div>
+                <Header title={'라인메이트 메인'} />
+                <div className="my_info">
+                    <div className="my_info_inner">
+                        <div className="img_area">
+                            <div className="none"></div>
                         </div>
-                        <div className='desc_area'>
-                            <div className='user_desc_area'>
-                                <h3 className='user_name'>Happy123</h3>
-                                <div className='join_date'>2024.01.24 JOIN</div>
+                        <div className="desc_area">
+                            <div className="user_desc_area">
+                                <h3 className="user_name">Happy123</h3>
+                                <div className="join_date">2024.01.24 JOIN</div>
                             </div>
-                            <div className='btns_area'>
+                            <div className="btns_area">
                                 <ul>
-                                    <li><TextButtonWithIcon type='text' classnames='top reservations' onclick={() => viewPage('reservations')} text={'My Events'} /></li>
-                                    <li><TextButtonWithIcon type='text' classnames='top qna' onclick={() => viewPage('qna')} text={'Q&A'} /></li>
-                                    <li><TextButtonWithIcon type='text' classnames='top like' onclick={() => viewPage('like')} text={'Like'} /></li>
-                                    <li><TextButtonWithIcon type='text' classnames='top review' onclick={() => viewPage('review')} text={'Review'} /></li>
+                                    <li>
+                                        <TextButtonWithIcon type="text" classnames="top reservations" onclick={() => viewPage('reservations')} text={'My Events'} />
+                                    </li>
+                                    <li>
+                                        <TextButtonWithIcon type="text" classnames="top qna" onclick={() => viewPage('qna')} text={'Q&A'} />
+                                    </li>
+                                    <li>
+                                        <TextButtonWithIcon type="text" classnames="top like" onclick={() => viewPage('like')} text={'Like'} />
+                                    </li>
+                                    <li>
+                                        <TextButtonWithIcon type="text" classnames="top review" onclick={() => viewPage('review')} text={'Review'} />
+                                    </li>
                                 </ul>
                             </div>
                         </div>
-
                     </div>
                 </div>
-                <div className='contents'>
-                    <div className='contents_inner'>
-                        <div className='side_menu'>
-                            <h3>
-                                MY PAGE
-                            </h3>
-                            <div className='menus'>
+                <div className="contents">
+                    <div className="contents_inner">
+                        <div className="side_menu">
+                            <h3>MY PAGE</h3>
+                            <div className="menus">
                                 <dl>
-                                    <dt><div>MY SETTING</div></dt>
+                                    <dt>
+                                        <div>MY SETTING</div>
+                                    </dt>
                                     <dd>
                                         <div>Profile</div>
                                     </dd>
@@ -55,19 +62,23 @@ export default function MypageLayout({ children }: { children: React.ReactNode }
                                     </dd>
                                 </dl>
                                 <dl>
-                                    <dt><div>PAYMENT</div></dt>
+                                    <dt>
+                                        <div>PAYMENT</div>
+                                    </dt>
                                 </dl>
                                 <dl>
-                                    <dt><div>NOTICE</div></dt>
+                                    <dt>
+                                        <div>NOTICE</div>
+                                    </dt>
                                 </dl>
                                 <dl>
-                                    <dt><div>LOGOUT</div></dt>
+                                    <dt>
+                                        <div>LOGOUT</div>
+                                    </dt>
                                 </dl>
                             </div>
                         </div>
-                        <div className='contents_area'>
-                            {children}
-                        </div>
+                        <div className="contents_area">{children}</div>
                     </div>
                 </div>
             </div>
