5530be729c6cbd9f0bc9f5906889356e93f373c9
윤지은 <yoonzeen@natecorp.com>
2026-04-08 13:37:33 +0900

    [MOD] 상세페이지 이미지 호출 관련 스크롤 함수 수정

--- src/app/program/[id]/_DetailContent.tsx
+++ src/app/program/[id]/_DetailContent.tsx
@@ -1,11 +1,9 @@
 'use client';
-import React from 'react';
+import React, { memo } from 'react';
 import 'styles/detailContent.scss';
 
-const DetailContent = ({html} : {html: string}) => {
-    return (
-        <div className='safe_detail_content' dangerouslySetInnerHTML={{ __html: html }} /> 
-    );
-};
+const DetailContent = memo(function DetailContent({ html }: { html: string }) {
+    return <div className="safe_detail_content" dangerouslySetInnerHTML={{ __html: html }} />;
+});
 
 export default DetailContent;
