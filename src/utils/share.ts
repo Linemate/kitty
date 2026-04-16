3a20b3491eafc26897bc8cf9e0ff406563892d5e
윤지은 <yoonzeen@natecorp.com>
2026-04-16 17:13:43 +0900

    [MOD] 카카오톡 공유하기 드디어 된다

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -12,51 +12,27 @@ export const shareProgram = async (str: string, program: programProps, completed
             const initialized = await initKakao();
             if (initialized && win.Kakao && win.Kakao.isInitialized && win.Kakao.isInitialized()) {
                 try {
-                    // Kakao.init('이 부분에 있는 키가 JAVASCRIPT 키가 맞는지 다시 한 번만 봐주세요!');
-
                     win.Kakao.Share.sendDefault({
                         objectType: 'feed',
                         content: {
-                            title: '도메인 테스트',
-                            description: '버튼아 나와라',
-                            imageUrl: 'https://k.kakaocdn.net/dn/bLi3QS/dJMb9bv3Ngg/07lwO1kANN5ZeCkNUmUpf1/kakaolink40_original.jpg',
+                            title: program.title,
+                            description: customDesc || program.title,
+                            imageUrl: program.thumbnail,
                             link: {
-                                mobileWebUrl: 'https://www.linemate.kr',
-                                webUrl: 'https://www.linemate.kr',
+                                mobileWebUrl: 'https://www.linemate.kr/program/' + program.id,
+                                webUrl: 'https://www.linemate.kr/program/' + program.id,
                             },
                         },
                         buttons: [
                             {
-                                title: '테스트 버튼',
+                                title: '라인메이트에서 보기',
                                 link: {
-                                    // 이 주소가 콘솔에 등록된 주소와 토씨 하나 안 틀리고 똑같아야 함
-                                    mobileWebUrl: 'https://www.linemate.kr',
-                                    webUrl: 'https://www.linemate.kr'
+                                    mobileWebUrl: 'https://www.linemate.kr/program/' + program.id,
+                                    webUrl: 'https://www.linemate.kr/program/' + program.id
                                 },
                             },
                         ],
                     });
-                    // win.Kakao.Share.sendDefault({
-                    //     objectType: 'feed',
-                    //     content: {
-                    //         title: program.title,
-                    //         description: customDesc || program.title,
-                    //         imageUrl: program.thumbnail,
-                    //         link: {
-                    //             mobileWebUrl: 'https://www.linemate.kr/program/' + program.id,
-                    //             webUrl: 'https://www.linemate.kr/program/' + program.id,
-                    //         },
-                    //     },
-                    //     buttons: [
-                    //         {
-                    //             title: '라인메이트에서 보기',
-                    //             link: {
-                    //                 mobileWebUrl: 'https://www.linemate.kr/program/' + program.id,
-                    //                 webUrl: 'https://www.linemate.kr/program/' + program.id
-                    //             },
-                    //         },
-                    //     ],
-                    // });
                 } catch (e) {
                     window.open(url, '_blank');
                 }
